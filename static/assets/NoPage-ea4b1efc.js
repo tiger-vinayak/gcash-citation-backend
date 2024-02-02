9cd583ae3196f247be2dbe5448630fb93482bb8b
Vinayak Gupta <vinayak.gupta@tigeranalytics.com>
2024-02-02 16:28:22 +0530

    Initial Commit

---
+++ static/assets/NoPage-ea4b1efc.js
@@ -0,0 +1,2 @@
+import{bE as o}from"./vendor-7810ac01.js";function e(){return o.jsx("h1",{children:"404"})}e.displayName="NoPage";export{e as Component};
+//# sourceMappingURL=NoPage-ea4b1efc.js.map
